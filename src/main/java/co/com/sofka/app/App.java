b7427a85c9ef0b30b49d8005ae048565cb29c38e
Sebastian Ruiz Gallego <sebasruigalle62@gmail.com>
2021-09-26 02:48:43 -0500

    All test finished

--- src/main/java/co/com/sofka/app/App.java
+++ src/main/java/co/com/sofka/app/App.java
@@ -17,34 +17,6 @@ public class App
     public static void main( String[] args ) throws Exception
     {
 
-        /*BufferedReader bufferedReader = new BufferedReader( new InputStreamReader( System.in ) );
-
-        System.out.println( "Enter number 1: " );
-        String textNumber1 = bufferedReader.readLine();
-
-        System.out.println( "Enter number 2: " );
-        String textNumber2 = bufferedReader.readLine();
-
-        Long number1 = Long.valueOf( textNumber1 );
-        Long number2 = Long.valueOf( textNumber2 );
-
-        BasicCalculator calculator = new BasicCalculator();
-        Long result = calculator.sum( number1, number2 );
-
-        System.out.println( number1 + " + " + number2 + " = " + result );
-
-        System.out.println( "Enter number 1: " );
-        String textNumber3 = bufferedReader.readLine();
-
-        System.out.println( "Enter number 2: " );
-        String textNumber4 = bufferedReader.readLine();
-
-        float number3 = Float.parseFloat( textNumber3 );
-        float number4 = Float.parseFloat( textNumber4 );
-
-        float result2 = calculator.divide( number3, number4 );
-
-        System.out.println( number3 + " + " + number4 + " = " + result2 );*/
         Menu menu = new Menu();
         menu.showMenu();
     }
--- src/main/java/co/com/sofka/app/menu/Menu.java
+++ src/main/java/co/com/sofka/app/menu/Menu.java
@@ -9,45 +9,65 @@ import java.nio.Buffer;
 
 public class Menu {
 
-    public void showMenu() throws IOException {
+    public void showMenu() throws Exception {
 
         String handleOption = "";
         BufferedReader bufferedReader = new BufferedReader( new InputStreamReader( System.in ) );
-        
+
         do {
 
 
-            System.out.println( "Enter number 1: " );
+            System.out.println( "Enter number:\n" +
+                    "1. To sum.\n" +
+                    "2. To subtract.\n" +
+                    "3. To multiply.\n" +
+                    "4. To divide.\n" +
+                    "0. To exit.\n" );
+
             handleOption = bufferedReader.readLine();
 
         }while (options(handleOption));
 
     }
 
-    private boolean options(String option){
+    private boolean options(String option) throws Exception {
+
+        BasicCalculator basicCalculator = new BasicCalculator();
 
         boolean isContinue = !option.equals("0");
 
         if(option.equals("1")){
 
+            System.out.println("El resultado es: " + basicCalculator.sum(requestNumber(),requestNumber()));
         }
 
         if(option.equals("2")){
 
+            System.out.println("El resultado es: " + basicCalculator.subtract(requestNumber(),requestNumber()));
         }
 
         if(option.equals("3")){
 
+            System.out.println("El resultado es: " + basicCalculator.multiply(requestNumber(),requestNumber()));
         }
 
         if(option.equals("4")){
 
-
+            System.out.println("El resultado es: " + basicCalculator.divide(requestNumber(),requestNumber()));
         }
 
         return isContinue;
     }
 
+    private Long requestNumber() throws IOException {
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader( System.in ) );
 
+        System.out.println( "Enter number: " );
+        String textNumber = bufferedReader.readLine();
+
+        Long number = Long.valueOf( textNumber );
+
+        return number;
+    }
 
 }
